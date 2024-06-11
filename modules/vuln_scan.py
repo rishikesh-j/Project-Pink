3f2ad34d774d00dbab15ae467ef42b40427b6633
Rishikesh J <71543790+rishikesh-j@users.noreply.github.com>
2024-06-12 00:06:56 +0530

    Update vuln_scan.py

--- modules/vuln_scan.py
+++ modules/vuln_scan.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-def run_nuclei(target_file, output_dir):
+def run_nuclei(target_file, output_dir, rate_limit, threads):
     output_file = os.path.join(output_dir, "nuclei_results.txt")
-    subprocess.run(['nuclei', '-l', target_file, '-o', output_file], check=True)
+    subprocess.run(['nuclei', '-silent', '-retries', '3', '-rl', str(rate_limit), '-c', str(threads), '-l', target_file, '-o', output_file], check=True)
     return output_file
