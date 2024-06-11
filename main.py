6e61cd9b69dd81452ef27435adc04ad553d6d3af
Rishikesh J <71543790+rishikesh-j@users.noreply.github.com>
2024-06-12 01:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import argparse
 from modules import sub_enum, vuln_scan
 
@@ -7,10 +8,10 @@ def create_output_directory():
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
 
-def process_domain(domain, output_dir):
+def process_domain(domain, output_dir, subfinder_threads, nuclei_rate_limit, nuclei_threads):
     print(f"Running Subdomain Enumeration for {domain}...")
     amass_output = sub_enum.run_amass(domain, output_dir)
-    subfinder_output = sub_enum.run_subfinder(domain, output_dir)
+    subfinder_output = sub_enum.run_subfinder(domain, output_dir, subfinder_threads)
     print(f"Amass results saved to {amass_output}")
     print(f"Subfinder results saved to {subfinder_output}")
 
@@ -22,7 +23,7 @@ def process_domain(domain, output_dir):
     print(f"Combined subdomains saved to {combined_subdomains_file}")
 
     print(f"Running Vulnerability Scanning for subdomains in {combined_subdomains_file}...")
-    nuclei_output = vuln_scan.run_nuclei(combined_subdomains_file, output_dir)
+    nuclei_output = vuln_scan.run_nuclei(combined_subdomains_file, output_dir, nuclei_rate_limit, nuclei_threads)
     print(f"Nuclei results saved to {nuclei_output}")
 
 def main():
@@ -31,15 +32,21 @@ def main():
     parser.add_argument('-l', '--list', type=str, help='File containing list of target domains')
     args = parser.parse_args()
 
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    
+    subfinder_threads = config.get('subfinder_threads', 10)
+    nuclei_rate_limit = config.get('nuclei_rate_limit', 20)
+    nuclei_threads = config.get('nuclei_threads', 5)
     output_dir = create_output_directory()
 
     if args.target:
-        process_domain(args.target, output_dir)
+        process_domain(args.target, output_dir, subfinder_threads, nuclei_rate_limit, nuclei_threads)
     elif args.list:
         with open(args.list, 'r') as file:
             domains = [line.strip() for line in file.readlines()]
             for domain in domains:
-                process_domain(domain, output_dir)
+                process_domain(domain, output_dir, subfinder_threads, nuclei_rate_limit, nuclei_threads)
     else:
         print("Please provide a target domain with -t or a list of domains with -l.")
 
