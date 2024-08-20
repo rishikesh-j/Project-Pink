fbeb56bc5b64f2e6eb8f53590fdcb3acecadc1f9
rishikesh-j <ubuntu@ip-172-21-19-95.ap-south-1.compute.internal>
2024-08-20 11:50:08 +0000

    Added Logging Capabilities

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import json
 import argparse
 from datetime import datetime
 from modules import sub_enum, vuln_scan, shodan_module, phishing, postman_leaks, github_leaks, network_scan, google_dork
-from utils.mongo_utils import save_to_mongo  # Import the mongo_utils module
+from utils.mongo_utils import save_to_mongo
+from utils.logging_utils import setup_logger
+
+# Set up the logger
+logger = setup_logger(__name__)
 
 def create_output_directory():
     output_dir = os.path.join(os.getcwd(), "Recon")
     os.makedirs(output_dir, exist_ok=True)
+    logger.info(f"Output directory created at: {output_dir}")
     return output_dir
 
 def process_domain(domain, organization, output_dir, config, github_org=None):
+    logger.info(f"Processing domain: {domain} for organization: {organization}")
+
     subfinder_threads = config['subfinder_threads']
     nuclei_rate_limit = config['nuclei_rate_limit']
     nuclei_threads = config['nuclei_threads']
@@ -19,16 +26,16 @@ def process_domain(domain, organization, output_dir, config, github_org=None):
     shodan_api_key = config['shodan_api_key']
     dnsx_threads = config['dnsx_threads']
 
-    print(f"Running Subdomain Enumeration for {domain}...")
+    logger.info("Running Subdomain Enumeration...")
     subfinder_output = sub_enum.run_subfinder(domain, output_dir, subfinder_threads)
-    print(f"Subfinder results saved to {subfinder_output}")
+    logger.info(f"Subfinder results saved to {subfinder_output}")
 
     combined_subdomains_file = sub_enum.combine_results(domain, output_dir, subfinder_output)
-    print(f"Combined subdomains saved to {combined_subdomains_file}")
+    logger.info(f"Combined subdomains saved to {combined_subdomains_file}")
 
-    print(f"Running Gotator for {domain}...")
+    logger.info("Running Gotator...")
     gotator_output = sub_enum.run_gotator(domain, combined_subdomains_file, output_dir)
-    print(f"Gotator results saved to {gotator_output}")
+    logger.info(f"Gotator results saved to {gotator_output}")
 
     final_subdomains_file = os.path.join(output_dir, f"{domain}_final_subdomains.txt")
     with open(final_subdomains_file, 'w') as outfile:
@@ -36,47 +43,47 @@ def process_domain(domain, organization, output_dir, config, github_org=None):
             outfile.write(infile.read())
         with open(gotator_output) as infile:
             outfile.write(infile.read())
-    print(f"Final subdomains file saved to {final_subdomains_file}")
+    logger.info(f"Final subdomains file saved to {final_subdomains_file}")
 
-    print(f"Running Subdomain Verification for {domain}...")
+    logger.info("Running Subdomain Verification...")
     httpx_output = sub_enum.run_httpx(domain, final_subdomains_file, output_dir, httpx_threads, httpx_rate_limit)
-    print(f"Httpx verified results saved to {httpx_output}")
+    logger.info(f"Httpx verified results saved to {httpx_output}")
 
-    print(f"Running Shodan Dorking for organization {organization}...")
+    logger.info("Running Shodan Dorking...")
     shodan_module.search_shodan(organization, output_dir, shodan_api_key)
-    print(f"Shodan results saved to the database")
+    logger.info("Shodan results saved to the database")
 
-    print(f"Running Phishing Domain Scan for {domain}...")
+    logger.info("Running Phishing Domain Scan...")
     phishing.phishing_scan(domain, output_dir)
-    print(f"Phishing scan results saved to the database")
+    logger.info("Phishing scan results saved to the database")
 
-    print(f"Running Postman Leaks Scan for {domain}...")
+    logger.info("Running Postman Leaks Scan...")
     postman_leaks.postman_leaks(domain, output_dir)
-    print(f"Postman leaks results saved to the database")
+    logger.info("Postman leaks results saved to the database")
 
     if github_org:
-        print(f"Running GitHub Leaks Scan for organization {github_org}...")
+        logger.info(f"Running GitHub Leaks Scan for organization {github_org}...")
         github_leaks.github_leaks(github_org, output_dir)
-        print(f"GitHub leaks results saved to the database")
+        logger.info("GitHub leaks results saved to the database")
 
-    print(f"Running Google Dorking for {domain}...")
+    logger.info("Running Google Dorking...")
     google_dork.run_google_dorks(domain)
-    print(f"Google Dorking results saved to the database")
+    logger.info("Google Dorking results saved to the database")
 
-    print(f"Running Network Scan for {domain}...")
+    logger.info("Running Network Scan...")
     network_scan.network_scan(domain, output_dir, config)
-    print(f"Network scan results saved to the database")
+    logger.info("Network scan results saved to the database")
 
-    print(f"Running Vulnerability Scanning for subdomains in {httpx_output}...")
+    logger.info("Running Vulnerability Scanning...")
     vuln_scan.run_nuclei(httpx_output, output_dir, nuclei_rate_limit, nuclei_threads)
-    print(f"Nuclei results saved to {output_dir}")
+    logger.info(f"Nuclei results saved to {output_dir}")
 
 def process_shodan(organization, output_dir, config):
     shodan_api_key = config['shodan_api_key']
 
-    print(f"Running Shodan Dorking for organization {organization}...")
+    logger.info(f"Running Shodan Dorking for organization: {organization}")
     shodan_module.search_shodan(organization, output_dir, shodan_api_key)
-    print(f"Shodan results saved to the database")
+    logger.info("Shodan results saved to the database")
 
 def main():
     parser = argparse.ArgumentParser(description='Recon script for subdomain enumeration, vulnerability scanning, and Shodan dorking.')
